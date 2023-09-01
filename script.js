c04ca785fcdb3c2ac884f50d53c93c7df4d392d4
GitHub Action <action@github.com>
2023-09-01 00:37:41 +0000

    Update daily quote Fri Sep  1 00:37:41 UTC 2023

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const quote = 'Quote 2: In the middle of every difficulty lies opportunity. - Albert Einstein';
+const quote = 'Life is 10% what happens to us and 90% how we react to it. - Charles R. Swindoll';
 document.getElementById('quote').textContent = quote;
