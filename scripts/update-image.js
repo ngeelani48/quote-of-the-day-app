c2c894206fd604a111956ec9278de3e4353232e6
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-09-02 08:17:53 +0300

    Added more images

--- scripts/update-image.js
+++ scripts/update-image.js
@@ -4,6 +4,10 @@ const IMAGES = [
   'image1.jpg',
   'image2.jpg',
   'image3.jpg',
+  'image4.jpg',
+  'image5.jpg',
+  'image6.jpg',
+  'image7.jpg',
 ];
 
 const newImage = IMAGES[new Date().getDay() % IMAGES.length];
