31eac49d18e2a6e1b9d9eb84baca66bb66d027da
Muhammed Nafees Geelani <engnrnafees@gmail.com>
2023-09-07 12:15:52 +0300

    Updated files

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const quote = 'Your time is limited, don't waste it living someone else's life. - Steve Jobs';
+const quote = `Your time is limited, don't waste it living someone else's life. - Steve Jobs`;
 document.getElementById('quote').textContent = quote;
