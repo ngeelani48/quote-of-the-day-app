fc16b3de56407116107616b85b7b47ec822b9ab0
GitHub Action <action@github.com>
2023-09-08 00:34:57 +0000

    Update daily quote Fri Sep  8 00:34:57 UTC 2023

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const quote = `Your time is limited, don't waste it living someone else's life. - Steve Jobs`;
+const quote = 'Life is 10% what happens to us and 90% how we react to it. - Charles R. Swindoll';
 document.getElementById('quote').textContent = quote;
