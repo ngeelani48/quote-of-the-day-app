f1d4cd1ca8a41175993ae552dfd2cc16e250d7cf
GitHub Action <action@github.com>
2023-10-23 00:36:28 +0000

    Update daily quote Mon Oct 23 00:36:28 UTC 2023

--- script.js
+++ script.js
@@ -1,2 +1,2 @@
-const quote = 'The only way to do great work is to love what you do. - Steve Jobs';
+const quote = 'Success is not final, failure is not fatal: It is the courage to continue that counts. - Winston Churchill';
 document.getElementById('quote').textContent = quote;
